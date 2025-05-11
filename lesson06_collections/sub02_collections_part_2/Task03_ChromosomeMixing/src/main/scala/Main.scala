1a12b9bc8c4006b79f2fc0bc0b4059808d13b436
st117171 <st117171@student.spbu.ru>
2025-05-11 14:13:01 +0300

    Fix Task03 in lesson06_collections/sub02_collections_part_2

--- lesson06_collections/sub02_collections_part_2/Task03_ChromosomeMixing/src/main/scala/Main.scala
+++ lesson06_collections/sub02_collections_part_2/Task03_ChromosomeMixing/src/main/scala/Main.scala
@@ -1,17 +1,39 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    val points: List[Int] = Lesson.points.sorted
-    var chr1: List[Char] = Lesson.chr1
-    var chr2: List[Char] = Lesson.chr2
+object Lesson {
+  def points1: List[Int] = List(1, 3)
+  def chr1_1: List[Char] = "xxxxx".toList
+  def chr2_1: List[Char] = "yyyyy".toList
 
-    for (point <- points) {
-      val (head1, tail1) = chr1.splitAt(point)
-      val (head2, tail2) = chr2.splitAt(point)
-      chr1 = head1 ++ tail2
-      chr2 = head2 ++ tail1
+  def points2: List[Int] = List(2, 4, 5)
+  def chr1_2: List[Char] = "aaaaaaa".toList
+  def chr2_2: List[Char] = "ddddddd".toList
+}
+
+object Main {
+  def performCrossover(points: List[Int], chr1: List[Char], chr2: List[Char]): (List[Char], List[Char]) = {
+    val sortedPoints = points.sorted
+    var c1 = chr1
+    var c2 = chr2
+
+    for (point <- sortedPoints) {
+      val (head1, tail1) = c1.splitAt(point)
+      val (head2, tail2) = c2.splitAt(point)
+      c1 = head1 ++ tail2
+      c2 = head2 ++ tail1
     }
 
-    println(chr1.mkString)
-    println(chr2.mkString)
+    (c1, c2)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val (res1_1, res1_2) = performCrossover(Lesson.points1, Lesson.chr1_1, Lesson.chr2_1)
+    println("Test 1:")
+    println(res1_1.mkString)  // должно быть "xyyxx"
+    println(res1_2.mkString)  // должно быть "yxxyy"
+    println()
+
+    val (res2_1, res2_2) = performCrossover(Lesson.points2, Lesson.chr1_2, Lesson.chr2_2)
+    println("Test 2:")
+    println(res2_1.mkString)  // должно быть "aaddadd"
+    println(res2_2.mkString)  // должно быть "ddaadaa"
   }
 }
